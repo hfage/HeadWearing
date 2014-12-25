0fac1dfc9986c3a04bb2d18ac7119c974b46c270
hfage <zhengfa.h@gmail.com>
2014-12-25 21:43:12 +0800

    增加了搜索蓝牙设备模块
    增加了搜索蓝牙设备模块，还有待优化

--- gen/com/example/headwearing/R.java
+++ gen/com/example/headwearing/R.java
@@ -13,12 +13,30 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int device_address=0x7f070001;
+        public static final int device_name=0x7f070000;
+        public static final int menu_refresh=0x7f070002;
+        public static final int menu_scan=0x7f070003;
+        public static final int menu_stop=0x7f070004;
+    }
     public static final class layout {
         public static final int activity_head_wear=0x7f030000;
+        public static final int listitem_device=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        /**  In BLEDevice.java 
+         */
+        public static final int ble_not_supported=0x7f040004;
+        public static final int error_bluetooth_not_supported=0x7f040005;
         public static final int hello_world=0x7f040001;
+        public static final int menu_scan=0x7f040002;
+        public static final int menu_stop=0x7f040003;
+        public static final int unknown_device=0x7f040006;
     }
     public static final class style {
         /** 
--- src/com/example/headwearing/HeadWear.java
+++ src/com/example/headwearing/HeadWear.java
@@ -1,15 +1,98 @@
 package com.example.headwearing;
 
+
+
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class HeadWear extends Activity {
-
+	public static boolean DEBUG = true;
+	public static boolean mBLEDeviceConnected = false;
+	public static boolean mBLEDeviceConnecting = true;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_head_wear);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if(mBLEDeviceConnected){
+			menu.findItem(R.id.menu_scan).setVisible(false);
+			menu.findItem(R.id.menu_stop).setVisible(true);
+		}
+		else{
+			menu.findItem(R.id.menu_scan).setVisible(true);
+			menu.findItem(R.id.menu_stop).setVisible(false);
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		switch(id){
+			case R.id.menu_scan:{
+				if(!mBLEDeviceConnected){
+					Intent intent = new Intent(this,BLEDevice.class);
+					startActivity(intent);
+				}
+				break;
+			}
+			case R.id.menu_stop:{
+				if(mBLEDeviceConnected){
+					
+				}
+				break;
+			}
+		}
+		invalidateOptionsMenu();
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new AlertDialog.Builder(this)
+                    // .setIcon(R.drawable.services)
+                    .setTitle("退出程序？")
+                    .setMessage("真的要退出吗？")
+                    .setNegativeButton("取消",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,int which) {
+                                	
+                                }
+                            })
+                    .setPositiveButton("确定",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    finish();
+                                }
+                            }).show();
+            return true;
+        }else{
+
+        	return super.onKeyDown(keyCode, event);
+        }
+    }
 }
